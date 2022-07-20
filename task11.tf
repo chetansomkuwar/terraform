a8b094c1a436247199883c2b40fa0126a3720271
chetan <chetansomkuwar45@gmail.com>
2022-07-20 13:21:21 +0530

    --- Auto Git Commit ---

--- task11.tf
+++ task11.tf
@@ -39,12 +39,12 @@ resource "aws_iam_group_membership" "adding_5_users_into_group111" {
   name = "tf-testing-group-membership"
 
   users = [
-    aws_iam_user.user11.name,
-    aws_iam_user.user12.name,
-    aws_iam_user.user13.name,
-    aws_iam_user.user14.name,
-    aws_iam_user.user15.name,
-    aws_iam_user.user16.name,
+    aws_iam_user.u1.name,
+    aws_iam_user.u2.name,
+    aws_iam_user.u3.name,
+    aws_iam_user.u4.name,
+    aws_iam_user.u5.name,
+    aws_iam_user.u6.name,
   ]
 
   group = aws_iam_group.group111.name
