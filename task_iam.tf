5867f479ceb664590950b432af30167a783f8131
chetan <chetansomkuwar45@gmail.com>
2022-07-20 11:00:23 +0530

    --- Auto Git Commit ---

--- task_iam.tf
+++ task_iam.tf
@@ -1,2 +1,6 @@
 # create 10 users, create demo & group and add this 10 users in that group
 
+variable "image_id" {
+  type        = string
+  description = "The id of the machine image (AMI) to use for the server."
+}
