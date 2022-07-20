f11f2fbe3b02fabef7e3971e047692234f7add70
chetan <chetansomkuwar45@gmail.com>
2022-07-20 16:14:26 +0530

    --- Auto Git Commit ---

--- task_create_ec2_instance.tf
+++ task_create_ec2_instance.tf
@@ -3,7 +3,30 @@
 resource "aws_instance" "this" {
   ami           = "ami-006d3995d3a6b963b"
   instance_type = "t2.micro"
-   }
+  # count = 5
+  security_groups = ["default"]
+  key_name        = root
+
+  tags = {
+    Name = "Terraform-instance"
+  }
+}
+
+#security group using Terraform
+
+resource "aws_security_group" "tf_sg" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-04e0aaa0c661fd0ac"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   ingress {
     description      = "HTTP"
