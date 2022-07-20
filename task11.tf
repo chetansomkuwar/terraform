7f8081bee0dbb00b85f49842048c97d47950c34b
chetan <chetansomkuwar45@gmail.com>
2022-07-20 13:08:56 +0530

    --- Auto Git Commit ---

--- task11.tf
+++ task11.tf
@@ -1,3 +1,23 @@
 resource "aws_iam_user" "u1" {
   name = "user11"
 }
+
+resource "aws_iam_user" "u2" {
+  name = "user12"
+}
+
+resource "aws_iam_user" "u3" {
+  name = "user13"
+}
+
+resource "aws_iam_user" "u4" {
+  name = "user14"
+}
+
+resource "aws_iam_user" "u1" {
+  name = "user11"
+}
+
+resource "aws_iam_user" "u1" {
+  name = "user11"
+}
