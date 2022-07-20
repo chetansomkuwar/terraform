616d70a158dfcfa7b5ffcc68d0a4c228e4f5d472
chetan <chetansomkuwar45@gmail.com>
2022-07-20 14:59:38 +0530

    --- Auto Git Commit ---

--- TASK_S3bucket_any_policy.tf
+++ TASK_S3bucket_any_policy.tf
@@ -22,8 +22,8 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
     }
 
     actions = [
-      "s3:GetObject",
-      "s3:ListBucket",
+      
+      
     ]
 
     resources = [
