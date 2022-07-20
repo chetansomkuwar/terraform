92da3868d6e79ed2821cd40a461b69b86c8000ce
chetan <chetansomkuwar45@gmail.com>
2022-07-20 13:09:36 +0530

    --- Auto Git Commit ---

--- task11.tf
+++ task11.tf
@@ -1,7 +1,8 @@
+#creat 10 users
 resource "aws_iam_user" "u1" {
   name = "user11"
 }
-
+5
 resource "aws_iam_user" "u2" {
   name = "user12"
 }
@@ -14,10 +15,11 @@ resource "aws_iam_user" "u4" {
   name = "user14"
 }
 
-resource "aws_iam_user" "u1" {
-  name = "user11"
+resource "aws_iam_user" "u5" {
+  name = "user15"
 }
 
-resource "aws_iam_user" "u1" {
-  name = "user11"
+resource "aws_iam_user" "u6" {
+  name = "user16"
 }
+
