7851753298e2fa0c96f2dd8a1881650ac982541a
chetan <chetansomkuwar45@gmail.com>
2022-07-20 16:22:39 +0530

    --- Auto Git Commit ---

--- task11.tf
+++ task11.tf
@@ -1,66 +1,66 @@
-#creat 10 users
-resource "aws_iam_user" "u1" {
-  name = "user11"
-}
+# #creat 10 users
+# resource "aws_iam_user" "u1" {
+#   name = "user11"
+# }
 
-resource "aws_iam_user" "u2" {
-  name = "user12"
-}
+# resource "aws_iam_user" "u2" {
+#   name = "user12"
+# }
 
-resource "aws_iam_user" "u3" {
-  name = "user13"
-}
+# resource "aws_iam_user" "u3" {
+#   name = "user13"
+# }
 
-resource "aws_iam_user" "u4" {
-  name = "user14"
-}
+# resource "aws_iam_user" "u4" {
+#   name = "user14"
+# }
 
-resource "aws_iam_user" "u5" {
-  name = "user15"
-}
+# resource "aws_iam_user" "u5" {
+#   name = "user15"
+# }
 
-resource "aws_iam_user" "u6" {
-  name = "user16"
-}
+# resource "aws_iam_user" "u6" {
+#   name = "user16"
+# }
 
-#create 2 groups
+# #create 2 groups
 
-resource "aws_iam_group" "g1" {
-  name = "group111"
-}
+# resource "aws_iam_group" "g1" {
+#   name = "group111"
+# }
 
-resource "aws_iam_group" "g2" {
-  name = "group222"
-}
+# resource "aws_iam_group" "g2" {
+#   name = "group222"
+# }
 
-#add five users into group
+# #add five users into group
 
-resource "aws_iam_group_membership" "adding_5_users_into_group111" {
-  name = "tf-testing-group-membership"
+# resource "aws_iam_group_membership" "adding_5_users_into_group111" {
+#   name = "tf-testing-group-membership"
 
-  users = [
-    aws_iam_user.u1.name,
-    aws_iam_user.u2.name,
-    aws_iam_user.u3.name,
-    aws_iam_user.u4.name,
-    aws_iam_user.u5.name,
-    aws_iam_user.u6.name,
-  ]
+#   users = [
+#     aws_iam_user.u1.name,
+#     aws_iam_user.u2.name,
+#     aws_iam_user.u3.name,
+#     aws_iam_user.u4.name,
+#     aws_iam_user.u5.name,
+#     aws_iam_user.u6.name,
+#   ]
 
-  group = aws_iam_group.g1.name
-}
+#   group = aws_iam_group.g1.name
+# }
 
-resource "aws_iam_group_membership" "adding_5_users_into_group222" {
-  name = "tf-testing-group-membership1"
+# resource "aws_iam_group_membership" "adding_5_users_into_group222" {
+#   name = "tf-testing-group-membership1"
 
-  users = [
-    aws_iam_user.u1.name,
-    aws_iam_user.u2.name,
-    aws_iam_user.u3.name,
-    aws_iam_user.u4.name,
-    aws_iam_user.u5.name,
-    aws_iam_user.u6.name,
-  ]
+#   users = [
+#     aws_iam_user.u1.name,
+#     aws_iam_user.u2.name,
+#     aws_iam_user.u3.name,
+#     aws_iam_user.u4.name,
+#     aws_iam_user.u5.name,
+#     aws_iam_user.u6.name,
+#   ]
 
-  group = aws_iam_group.g2.name
-}
+#   group = aws_iam_group.g2.name
+# }
