32e33c484500c8d19fc20a1b4447dee06d1b4ac3
chetan <chetansomkuwar45@gmail.com>
2022-07-20 11:02:54 +0530

    --- Auto Git Commit ---

--- task_iam.tf
+++ task_iam.tf
@@ -1,5 +1,16 @@
-# create 10 users, create demo & group and add this 10 users in that group
+# create 10 users, create demo & devil and add this 10 users in that group
 
 resource "aws_iam_group" "group1" {
   name = "demo"
+}
+
+resource "aws_iam_group" "group2" {
+  name = "devil"
+}
+
+resource "aws_iam_user" "add_user_2" {
+    name = "thor"
+    tags = {
+        tag-key = "tony-stark1"
+    }
 }
