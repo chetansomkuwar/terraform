bfc69c10c7f0ea3c9a2ede784f604b7441fd3337
chetan <chetansomkuwar45@gmail.com>
2022-07-20 15:23:07 +0530

    --- Auto Git Commit ---

--- task_create_ec2_instance.tf
+++ task_create_ec2_instance.tf
@@ -1,61 +1,61 @@
 # #terraform code to launch ec2 instance
 
-# resource "aws_instance" "practice" {
-#   ami           = "ami-0fb653ca2d3203ac1"
-#   instance_type = "t2.micro"
-# # count = 5
-#   security_groups = ["security group using Terraform"]
+resource "aws_instance" "practice" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+# count = 5
+  security_groups = ["security group using Terraform"]
 
-#   tags = {
-#     Name = "Terraform-instance"
-#   }
-# }
+  tags = {
+    Name = "Terraform-instance"
+  }
+}
 
-# #security group using Terraform
+#security group using Terraform
 
-# resource "aws_security_group" "tf_sg" {
-#   name        = "security group using Terraform"
-#   description = "security group using Terraform"
-#   vpc_id      = "vpc-08f53a580588af642"
+resource "aws_security_group" "tf_sg" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = "vpc-08f53a580588af642"
 
-#   ingress {
-#     description      = "HTTPS"
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#     ingress {
-#     description      = "HTTP"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+    ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#     ingress {
-#     description      = "SSH"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+    ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "tf_sg"
-#   }
-# }
+  tags = {
+    Name = "tf_sg"
+  }
+}
 
-# #create & attach key pair to the instance
+#create & attach key pair to the instance
