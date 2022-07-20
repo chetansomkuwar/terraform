afb1a0244f0fe98ece27c11c488494ce1b683eb1
chetan <chetansomkuwar45@gmail.com>
2022-07-20 11:42:12 +0530

    --- Auto Git Commit ---

--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 #variables.tf
 variable "username" {
   type = "list"
-  default = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"] }
+  default = ["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10"] 
+  
+  }
