f4fc76dc7abd7f3f8b91aa6c4f1ec3dc05273405
chetan <chetansomkuwar45@gmail.com>
2022-07-20 14:46:04 +0530

    --- Auto Git Commit ---

--- TASK_S3bucket_any_policy.tf
+++ TASK_S3bucket_any_policy.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "allow_access_from_another_account" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["123456789012"]
+      identifiers = [""]
     }
 
     actions = [
