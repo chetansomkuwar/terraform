b5be5f7ccafb3c1f75879453e87fc1941e250726
chetan <chetansomkuwar45@gmail.com>
2022-07-21 14:26:05 +0530

    --- Auto Git Commit ---

---
+++ users.tf
@@ -0,0 +1,48 @@
+############ varibale examples files ###############
+# resource "aws_iam_user" "reference1" {
+#   name = var.string_var
+#   tags = {
+#     tag-key = "new-user1"
+#   }
+# }
+
+# #Note:- this user calling name --> variable string --> string variable provide "sandesh"    
+
+# resource "aws_iam_user" "reference2" {
+#   name = var.number_var
+#   tags = {
+#     tag-key = "new-user2"
+#   }
+# }
+
+#Note:- this resource calling --> number variable --> thier provided no
+
+# resource "aws_iam_user" "reference3" {
+#   name = var.list_var[3]
+#   tags = {
+#     tag-key = "new-user3"
+#   }
+# }
+
+#Note:- this resource calling --> list variable --> thier provided list of username
+
+# resource "aws_iam_user" "reference4" {
+#   name = var.map_var.u2
+#   # here u2 mention after the name & u2= find variable in variabl files.
+#   tags = {
+#     tag-key = "new-user3"
+#   }
+# }
+
+#Note:- in map resource u2,u3,u1 call variable map
+
+# resource "aws_iam_user" "reference2" {
+#   name = var.any_var.u4.u3
+#   tags = {
+#     tag-key = "komal-giradkar"
+#   }
+# }
+
+#Note:- u4.u3 means in "any variable" --> select "u4=map" --> choose rahul
+
+
