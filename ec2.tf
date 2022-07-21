9233471e120edf76f1a4374081071ae27425982c
chetan <chetansomkuwar45@gmail.com>
2022-07-21 15:28:11 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,9 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-ebd02392"
+  ami                    = "ami-0cff7528ff583bf9a"
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "root"
   monitoring             = true
   vpc_security_group_ids = ["sg-12345678"]
   subnet_id              = "subnet-eddcdzz4"
