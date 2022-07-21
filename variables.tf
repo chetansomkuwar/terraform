cd1ffbebbcae250825fe7d01f8641dd5fe473609
chetan <chetansomkuwar45@gmail.com>
2022-07-21 13:09:11 +0530

    --- Auto Git Commit ---

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@
 #### variable for list ##### --> call listing
 variable "list_var" {
   type    = list(any)
-  default = [shubham, pranay, atharv, sand4]
+  default = [shubham, pranay, atharv, akash]
   #               0         1         2         3
   description = "variables details through list"
 }
