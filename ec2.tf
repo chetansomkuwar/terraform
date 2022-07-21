a6cd96ee643d42ce9f8d56576126ee72ac208506
chetan <chetansomkuwar45@gmail.com>
2022-07-21 15:59:35 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -28,7 +28,6 @@ module "vpc" {
 
   azs             = ["eu-west-1a"]
   private_subnets = ["128.0.0.0"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
