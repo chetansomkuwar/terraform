1a57dcd06e0688011efe40d32af948057a1297c6
chetan <chetansomkuwar45@gmail.com>
2022-07-21 15:59:04 +0530

    --- Auto Git Commit ---

--- ec2.tf
+++ ec2.tf
@@ -26,8 +26,8 @@ module "vpc" {
   name = "jarvis-vpc"
   cidr = "172.50.0.10/24"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["128.0.0.0", "10.0.2.0/24", "10.0.3.0/24"]
+  azs             = ["eu-west-1a"]
+  private_subnets = ["128.0.0.0"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
