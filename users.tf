04990b2c463d77f105dbbf2791a979c6b916c39f
chetan <chetansomkuwar45@gmail.com>
2022-07-21 12:56:26 +0530

    --- Auto Git Commit ---

--- users.tf
+++ users.tf
@@ -9,7 +9,7 @@ resource "aws_iam_user" "reference1" {
 #Note:- this user calling name --> variable string --> string variable provide "sandesh"    
 
 resource "aws_iam_user" "reference2" {
-  count  = var.number_var
+  count = var.number_var
   tags = {
     tag-key = "new-user2"
   }
