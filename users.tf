6276ac90e36e0200a36bfa55c330bff2bbe41f9f
chetan <chetansomkuwar45@gmail.com>
2022-07-21 11:33:12 +0530

    --- Auto Git Commit ---

---
+++ users.tf
@@ -0,0 +1,11 @@
+############ varibale examples files ###############
+resource "aws_iam_user" "reference1" {
+  name = var.string_var
+  tags = {
+    tag-key = "new-user1"
+  }
+}
+
+        #Note:- this 
+
+    
