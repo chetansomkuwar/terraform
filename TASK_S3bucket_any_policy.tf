20074d5da4ea46f72aee6259cc43f5c7c5ed3ade
chetan <chetansomkuwar45@gmail.com>
2022-07-21 10:25:34 +0530

    --- Auto Git Commit ---

--- TASK_S3bucket_any_policy.tf
+++ TASK_S3bucket_any_policy.tf
@@ -1,57 +1,57 @@
-# # create S3 bucket
+# create S3 bucket
 
-# resource "aws_s3_bucket" "B1" {
-#   bucket = "jarvisbuckk"
+resource "aws_s3_bucket" "B1" {
+  bucket = "jarvisbuckk"
 
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
-# }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
-# resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-#   bucket = aws_s3_bucket.B1.id
-#   policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-# }
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.B1.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+}
 
-# data "aws_iam_policy_document" "allow_access_from_another_account" {
-#   statement {
-#     principals {
-#       type        = "AWS"
-#       identifiers = ["829912339674"]
-#     }
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["829912339674"]
+    }
 
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Sid": "VisualEditor0",
-#             "Effect": "Allow",
-#             "Action": [
-#                 "s3:ListStorageLensConfigurations",
-#                 "s3:ListAccessPointsForObjectLambda",
-#                 "s3:GetAccessPoint",
-#                 "s3:PutAccountPublicAccessBlock",
-#                 "s3:GetAccountPublicAccessBlock",
-#                 "s3:ListAllMyBuckets",
-#                 "s3:ListAccessPoints",
-#                 "s3:PutAccessPointPublicAccessBlock",
-#                 "s3:ListJobs",
-#                 "s3:PutStorageLensConfiguration",
-#                 "s3:ListMultiRegionAccessPoints",
-#                 "s3:CreateJob"
-#             ],
-#             "Resource": "*"
-#         },
-#         {
-#             "Sid": "VisualEditor1",
-#             "Effect": "Allow",
-#             "Action": "s3:*",
-#             "Resource": [
-#                 "arn:aws:s3:::jarvisbuckk/ubuntt.txt",
-#                 "arn:aws:s3:::jarvisbuckk/India_team (1).bkp",
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListStorageLensConfigurations",
+                "s3:ListAccessPointsForObjectLambda",
+                "s3:GetAccessPoint",
+                "s3:PutAccountPublicAccessBlock",
+                "s3:GetAccountPublicAccessBlock",
+                "s3:ListAllMyBuckets",
+                "s3:ListAccessPoints",
+                "s3:PutAccessPointPublicAccessBlock",
+                "s3:ListJobs",
+                "s3:PutStorageLensConfiguration",
+                "s3:ListMultiRegionAccessPoints",
+                "s3:CreateJob"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "s3:*",
+            "Resource": [
+                "arn:aws:s3:::jarvisbuckk/ubuntt.txt",
+                "arn:aws:s3:::jarvisbuckk/India_team (1).bkp",
 
-#             ]
-#         }
-#     ]
-# }
+            ]
+        }
+    ]
+}
