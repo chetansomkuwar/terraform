8c8823153ffd64267842c48c34e32d7c9b2a30c1
chetan <chetansomkuwar45@gmail.com>
2022-07-25 19:15:23 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  # Configuration options
+  region  = "us-east-1"
+  profile = "jarvis"
+}
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
