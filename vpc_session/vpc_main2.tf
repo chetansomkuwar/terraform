9b7d31eac79706df3237919ba9a86cb0e6c283fa
chetan <chetansomkuwar45@gmail.com>
2022-07-25 19:58:40 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -7,10 +7,6 @@ resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr_block
   instance_tenancy = "default"
   tags = var.tag
-
-   tags = {
-    Name = "my_first_vpc"
-  }
 }
 
 resource "aws_subnet" "public_subnet" {
