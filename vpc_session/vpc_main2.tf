3d9a99e70fe33bbae1677a1551769dadf5bb8df9
chetan <chetansomkuwar45@gmail.com>
2022-07-25 21:39:13 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -3,19 +3,28 @@ provider "aws" {
   region  = "us-east-1"
   profile = "jarvis"
 }
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr_block
+# resource "aws_instance" "demo" {
+#   ami           = "ami-0cff7528ff583bf9a" # us-west-2
+#   instance_type = "t2.micro"
+# }
+resource "aws_vpc" "this" {
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-  tags = var.tag 
+#   tags = var.tag
+  #tags = merge(var.tag, {"environment" = var.env}, {"owner" = var.owner})
+   tags = merge(var.tag, {"owner" = format("%s_%s", var.env, var.owner)})
 }
-
-resource "aws_subnet" "public_subnet" {
-  count = lengh(var.public_subnet)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet[count.index]
+resource "aws_subnet" "public" {
+    count = length(var.public_vpc_cidr)
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.public_vpc_cidr[count.index]
   availability_zone = element(var.availability_zone, count.index)
-  tags = "${var.tag}_public_subnet"
-  tags = {
-    Name = "Main"
-  }
+  tags = {"environment" = var.env}
+}
+resource "aws_subnet" "private" {
+    count = length(var.private_vpc_cidr)
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.private_vpc_cidr[count.index]
+  availability_zone = element(var.availability_zone, count.index)
+  tags = {"environment" = var.env}
 }
