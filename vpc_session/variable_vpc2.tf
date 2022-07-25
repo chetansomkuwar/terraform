d98e9d12a5f491275503ce2fe007e0d99bf5f3c2
chetan <chetansomkuwar45@gmail.com>
2022-07-25 20:08:39 +0530

    --- Auto Git Commit ---

--- vpc_session/variable_vpc2.tf
+++ vpc_session/variable_vpc2.tf
@@ -1,16 +1,15 @@
 variable "vpc_cidr_block" {
   description = "cidr of vpc"
   type        = string
-}variable "tag" {
+}
+variable "tag" {
   description = "vpc tag via variable"
   type        = map(string)
 }
-
 variable "public_subnet" {
   description = "calling public subnet via variable"
   type        = list(string)
 }
-
 variable "availability_zone" {
   description = "availability_zone via variable"
   type        = list(string)
