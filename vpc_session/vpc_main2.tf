7d17f1bd00b0c0f1a6fe57db625033a9eb80cafc
chetan <chetansomkuwar45@gmail.com>
2022-07-25 20:06:25 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet[count.index]
   availability_zone = element(var.availability_zone, count.index)
-  tags = "${var.tag}_prod"
+  tags = "${var.tag}_public_subnet"
   tags = {
     Name = "Main"
   }
