181fd738d4cfdeafc1707dc590565c8561960588
chetan <chetansomkuwar45@gmail.com>
2022-07-26 14:15:46 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -36,3 +36,21 @@ resource "aws_vpc_gateway" "vpn_gw" {
   #   Name = "main"
   # }
 }
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
