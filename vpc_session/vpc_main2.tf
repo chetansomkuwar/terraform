0b878f265a0f8ae018df2d9ea333a04129371109
chetan <chetansomkuwar45@gmail.com>
2022-07-26 14:49:07 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -52,14 +52,14 @@ resource "aws_route_table" "public_rt" {
 }
 
 # create private route table 
-resource "aws_route_table" "private" {
+resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.this.id
     route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
-    Name = "public"
+    Name = "private_rt"
   }
 }
 
