b6ae097d4c2cbff3e2a669c963f986cfbf8acc92
chetan <chetansomkuwar45@gmail.com>
2022-07-26 15:04:32 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -69,7 +69,7 @@ resource "aws_route" "pvt_route_table" {
   gateway_id = aws_internet_gateway.igw.id
 }
 
-
+#create 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.foo.id
   route_table_id = aws_route_table.bar.id
