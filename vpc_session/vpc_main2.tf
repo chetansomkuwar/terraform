2d585872ec955ceb28f664ce9cc6f8a3f978db3c
chetan <chetansomkuwar45@gmail.com>
2022-07-26 14:21:59 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -41,7 +41,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
     route {
     cidr_block = "0.0.0.0/0"
-  #  gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.example.id
   }
   tags = {
     Name = "example"
