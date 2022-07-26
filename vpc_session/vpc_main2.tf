63995f6d2614bc817774aa4d4cada40dfd620ae2
chetan <chetansomkuwar45@gmail.com>
2022-07-26 14:47:17 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -51,4 +51,10 @@ resource "aws_route_table" "public" {
   }
 }
 
-# create privateroute table and add route entry from outside using another resource
+# create private route table and add route entry from outside using another resource
+resource "aws_route" "r" {
+  route_table_id            = "rtb-4fbb3ac4"
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+  depends_on                = [aws_route_table.testing]
+}
