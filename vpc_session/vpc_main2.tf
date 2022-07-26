a72b339b55ecd1a19d8cfb2eabe40089179c4582
chetan <chetansomkuwar45@gmail.com>
2022-07-26 14:27:50 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -39,7 +39,7 @@ resource "aws_vpc_gateway" "igw" {
 }
 
 
-#create route table
+#create route table --> attach to vpc --> do igw entry
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
     route {
