73b983c74f617986a30b4e488001c07ea1448106
chetan <chetansomkuwar45@gmail.com>
2022-07-26 15:35:54 +0530

    --- Auto Git Commit ---

--- vpc_session/vpc_main2.tf
+++ vpc_session/vpc_main2.tf
@@ -87,5 +87,5 @@ resource "aws_nat_gateway" "nat_gwa" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway" "igw]
+  depends_on = [aws_internet_gateway.igw]
 }
