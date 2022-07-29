4fc275894a84bd234bcee75b160b270617d8c4be
chetan <chetansomkuwar45@gmail.com>
2022-07-29 15:01:18 +0530

    --- Auto Git Commit ---

--- autoscaling_loadbalancer/autoscaling_loadbalancer.tf
+++ autoscaling_loadbalancer/autoscaling_loadbalancer.tf
@@ -20,9 +20,9 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = "vpc-0a807348fb2cec222"
-  subnets            = ["subnet-04c97d3e3d58741fe", "subnet-0129688326e642a66"]
-  security_groups    = ["sg-0ee859fd6cfcacc26", "sg-014cbb5be97e19501"]
+  vpc_id          = "vpc-0a807348fb2cec222"
+  subnets         = ["subnet-04c97d3e3d58741fe", "subnet-0129688326e642a66"]
+  security_groups = ["sg-0ee859fd6cfcacc26", "sg-014cbb5be97e19501"]
 
   access_logs = {
     bucket = "my-alb-logs"
@@ -37,11 +37,11 @@ module "alb" {
       targets = {
         my_target = {
           target_id = "i-0123456789abcdefg"
-          port = 80
+          port      = 80
         }
         my_other_target = {
           target_id = "i-a1b2c3d4e5f6g7h8i"
-          port = 8080
+          port      = 8080
         }
       }
     }
