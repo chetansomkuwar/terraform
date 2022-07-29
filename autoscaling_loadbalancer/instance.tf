0fb741ba20870e3c9491e2fdfb4751702ec0b897
chetan <chetansomkuwar45@gmail.com>
2022-07-29 22:27:03 +0530

    --- Auto Git Commit ---

---
+++ autoscaling_loadbalancer/instance.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "web-server" {
+  ami           = "ami-0f7559f51d3a22167"
+  instance_type =  "t2.micro"
+  count         =  2
+  key_name = "paris-key"
+  security_groups = ["${aws_security_group.my_sg.name}"]
+  user_data =  <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo systemctl reload apache2
+sudo rm -rf /var/www/html/index.html
+sudo echo "hello everyone $HOSTNAME" >> /var/www/html/index.html
+EOF
+
+ 
+
+   
+}
+
+
+
+
+
+
+
+
