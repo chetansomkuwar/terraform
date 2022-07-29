9792fa71c59645cdc5cee4534d488696c2d6988b
chetan <chetansomkuwar45@gmail.com>
2022-07-29 14:15:15 +0530

    --- Auto Git Commit ---

--- vpc_session/variable_vpc2.tf
+++ vpc_session/variable_vpc2.tf
@@ -1,21 +1,21 @@
-variable "vpc_cidr" {
-  type = string
-}
-variable "tag" {
-  type = map(string)
-}
-variable "public_vpc_cidr" {
-  type = list(string)
-}
-variable "private_vpc_cidr" {
-  type = list(string)
-}
-variable "availability_zone" {
-  type = list(string)
-}
-variable "env" {
-  type = string
-}
-variable "owner" {
-  type = string
-}
+# variable "vpc_cidr" {
+#   type = string
+# }
+# variable "tag" {
+#   type = map(string)
+# }
+# variable "public_vpc_cidr" {
+#   type = list(string)
+# }
+# variable "private_vpc_cidr" {
+#   type = list(string)
+# }
+# variable "availability_zone" {
+#   type = list(string)
+# }
+# variable "env" {
+#   type = string
+# }
+# variable "owner" {
+#   type = string
+# }
--- vpc_session/variables_data.auto.tfvars
+++ vpc_session/variables_data.auto.tfvars
@@ -1,9 +1,9 @@
-vpc_cidr          = "10.0.0.0/16"
-public_vpc_cidr   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
-availability_zone = ["us-east-1a", "us-east-1b"]
-tag = {
-  purpose = "my_vpc"
-}
-env              = "UAT"
-owner            = "localuser"
-private_vpc_cidr = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
+# vpc_cidr          = "10.0.0.0/16"
+# public_vpc_cidr   = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+# availability_zone = ["us-east-1a", "us-east-1b"]
+# tag = {
+#   purpose = "my_vpc"
+# }
+# env              = "UAT"
+# owner            = "localuser"
+# private_vpc_cidr = ["10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
