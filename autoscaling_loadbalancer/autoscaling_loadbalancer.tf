f1f47350e8a92d4230784fe8d30dddec6ae435e6
chetan <chetansomkuwar45@gmail.com>
2022-07-29 14:47:52 +0530

    --- Auto Git Commit ---

--- autoscaling_loadbalancer/autoscaling_loadbalancer.tf
+++ autoscaling_loadbalancer/autoscaling_loadbalancer.tf
@@ -21,7 +21,7 @@ module "alb" {
   load_balancer_type = "application"
 
   vpc_id             = "vpc-0a807348fb2cec222"
-  subnets            = ["subnet-abcde012", "subnet-bcde012a"]
+  subnets            = ["subnet-04c97d3e3d58741fe", "subnet-bcde012a"]
   security_groups    = ["sg-edcd9784", "sg-edcd9785"]
 
   access_logs = {
