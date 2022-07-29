6318bab9540d86920e113960f172583cfb14432d
chetan <chetansomkuwar45@gmail.com>
2022-07-29 14:23:42 +0530

    --- Auto Git Commit ---

--- autoscaling_loadbalancer/autoscaling_loadbalancer.tf
+++ autoscaling_loadbalancer/autoscaling_loadbalancer.tf
@@ -3,3 +3,14 @@ data "aws_availability_zones" "available" {}
 #  state = "available"
 
 # defime AMI
+resource "aws_ami" "example" {
+  name                = "terraform-example"
+  virtualization_type = "hvm"
+  root_device_name    = "/dev/xvda"
+
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    snapshot_id = "snap-xxxxxxxx"
+    volume_size = 8
+  }
+}
