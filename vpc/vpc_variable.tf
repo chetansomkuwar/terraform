812fb975c88ac9b5bb0d73147354ad34f30f5006
chetan <chetansomkuwar45@gmail.com>
2022-07-29 14:16:50 +0530

    --- Auto Git Commit ---

--- vpc/vpc_variable.tf
+++ vpc/vpc_variable.tf
@@ -1,41 +1,41 @@
-variable "vpc-cidr" {
-#  default     = "10.0.0.0/16"
-  description = "VPC CIDR Block"
-  type        = string
-}
+# variable "vpc-cidr" {
+# #  default     = "10.0.0.0/16"
+#   description = "VPC CIDR Block"
+#   type        = string
+# }
 
-variable "public-subnet-1-cidr" {
-#  default     = "10.0.0.0/24"
-  description = "Public Subnet 1 CIDR Block"
-  type        = string
-}
+# variable "public-subnet-1-cidr" {
+# #  default     = "10.0.0.0/24"
+#   description = "Public Subnet 1 CIDR Block"
+#   type        = string
+# }
 
-variable "public-subnet-2-cidr" {
-#  default     = "10.0.1.0/24"
-  description = "Public Subnet 2 CIDR Block"
-  type        = string
-}
+# variable "public-subnet-2-cidr" {
+# #  default     = "10.0.1.0/24"
+#   description = "Public Subnet 2 CIDR Block"
+#   type        = string
+# }
 
-variable "private-subnet-1-cidr" {
-#  default     = "10.0.2.0/24"
-  description = "Private Subnet 1 CIDR Block"
-  type        = string
-}
+# variable "private-subnet-1-cidr" {
+# #  default     = "10.0.2.0/24"
+#   description = "Private Subnet 1 CIDR Block"
+#   type        = string
+# }
 
-variable "private-subnet-2-cidr" {
-#  default     = "10.0.3.0/24"
-  description = "Private Subnet 2 CIDR Block"
-  type        = string
-}
+# variable "private-subnet-2-cidr" {
+# #  default     = "10.0.3.0/24"
+#   description = "Private Subnet 2 CIDR Block"
+#   type        = string
+# }
 
-variable "private-subnet-3-cidr" {
-#  default     = "10.0.4.0/24"
-  description = "Private Subnet 3 CIDR Block"
-  type        = string
-}
+# variable "private-subnet-3-cidr" {
+# #  default     = "10.0.4.0/24"
+#   description = "Private Subnet 3 CIDR Block"
+#   type        = string
+# }
 
-variable "private-subnet-4-cidr" {
-#  default     = "10.0.5.0/24"
-  description = "Private Subnet 4 CIDR Block"
-  type        = string
-}
+# variable "private-subnet-4-cidr" {
+# #  default     = "10.0.5.0/24"
+#   description = "Private Subnet 4 CIDR Block"
+#   type        = string
+# }
