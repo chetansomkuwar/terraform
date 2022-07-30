4b042a6607eaf0517a148cb95166199eafd23099
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:41:35 +0530

    --- Auto Git Commit ---

--- server.tf
+++ server.tf
@@ -36,6 +36,22 @@ sudo echo "hello everyone, this is mobile page $HOSTNAME" >> /var/www/html/index
 EOF
 }
 
+resource "aws_instance" "web-server" {
+  ami             = "ami-052efd3df9dad4825"
+  instance_type   = "t2.micro"
+  count           = 2
+  key_name        = "jarvis_privatekey"
+  security_groups = ["${aws_security_group.my_sg.name}"]
+  user_data       = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo systemctl reload apache2
+sudo rm -rf /var/www/html/index.html
+sudo echo "hello everyone, this is mobile page $HOSTNAME" >> /var/www/html/index.html
+EOF
+}
+
 
 
 
