15752d704b34a2569f9995440cf34143172d91c5
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:42:45 +0530

    --- Auto Git Commit ---

--- sg.tf
+++ sg.tf
@@ -10,7 +10,14 @@ resource "aws_security_group" "my_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port        = 0
