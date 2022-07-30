faab989bfcf129770859bdd17d0e0fa8b23a7cb3
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:42:15 +0530

    --- Auto Git Commit ---

---
+++ sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "my_sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0d38f6e75bce4c3e6"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
