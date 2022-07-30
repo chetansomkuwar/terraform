bdae17fa9ad6753b8040ea812d6939eb68acb275
chetan <chetansomkuwar45@gmail.com>
2022-07-30 12:49:45 +0530

    --- Auto Git Commit ---

--- server.tf
+++ server.tf
@@ -12,13 +12,12 @@ resource "aws_instance" "web-server" {
   key_name        = "jarvis_privatekey"
   security_groups = ["${aws_security_group.my_sg.name}"]
   user_data       = <<EOF
-#! /bin/bash
+#!/bin/bash
 sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl reload apache2
-sudo rm -rf /var/www/html/index.html
-sudo echo "hello everyone, this is laptop page $HOSTNAME" >> /var/www/html/index.html
-EOF
+rm -rfv /var/www/html/index.html
+sudo echo "hello everyone, this is my laptop page $HOSTNAME" >> /var/www/html/index.htmlEOF
 }
 
 resource "aws_instance" "web-server" {
