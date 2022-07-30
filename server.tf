976f4e6ec92ed433ab8f32d40801a638f756e0fa
chetan <chetansomkuwar45@gmail.com>
2022-07-30 14:42:23 +0530

    --- Auto Git Commit ---

--- server.tf
+++ server.tf
@@ -1,59 +1,59 @@
-provider "aws" {
-  # Configuration options
-  region  = "us-east-1"
-  profile = "jarvis"
-}
+# provider "aws" {
+#   # Configuration options
+#   region  = "us-east-1"
+#   profile = "jarvis"
+# }
 
-resource "aws_instance" "web-server" {
-  ami             = "ami-052efd3df9dad4825"
-  instance_type   = "t2.micro"
-  count           = 2
-  availability_zone = us-east-1a
-  key_name        = "jarvis_privatekey"
-  security_groups = ["${aws_security_group.my_sg.name}"]
-  user_data       = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install apache2 -y
-sudo systemctl reload apache2
-rm -rfv /var/www/html/index.html
-sudo echo "hello everyone, this is my laptop page $HOSTNAME" >> /var/www/html/index.html
-EOF
-}
+# resource "aws_instance" "web-server" {
+#   ami             = "ami-052efd3df9dad4825"
+#   instance_type   = "t2.micro"
+#   count           = 2
+#   availability_zone = us-east-1a
+#   key_name        = "jarvis_privatekey"
+#   security_groups = ["${aws_security_group.my_sg.name}"]
+#   user_data       = <<EOF
+# #!/bin/bash
+# sudo apt-get update
+# sudo apt-get install apache2 -y
+# sudo systemctl reload apache2
+# rm -rfv /var/www/html/index.html
+# sudo echo "hello everyone, this is my laptop page $HOSTNAME" >> /var/www/html/index.html
+# EOF
+# }
 
-resource "aws_instance" "web-server" {
-  ami             = "ami-052efd3df9dad4825"
-  instance_type   = "t2.micro"
-  count           = 2
-  availability_zone = us-east-1a
-  key_name        = "jarvis_privatekey"
-  security_groups = ["${aws_security_group.my_sg.name}"]
-  user_data       = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install apache2 -y
-sudo systemctl reload apache2
-sudo mkdir -p /var/www/html/mobile/
-sudo echo "hello world, welcome to my mobile page $HOSTNAME" >> /var/www/html/mobile/index.html
-EOF
-}
+# resource "aws_instance" "web-server" {
+#   ami             = "ami-052efd3df9dad4825"
+#   instance_type   = "t2.micro"
+#   count           = 2
+#   availability_zone = us-east-1a
+#   key_name        = "jarvis_privatekey"
+#   security_groups = ["${aws_security_group.my_sg.name}"]
+#   user_data       = <<EOF
+# #!/bin/bash
+# sudo apt-get update
+# sudo apt-get install apache2 -y
+# sudo systemctl reload apache2
+# sudo mkdir -p /var/www/html/mobile/
+# sudo echo "hello world, welcome to my mobile page $HOSTNAME" >> /var/www/html/mobile/index.html
+# EOF
+# }
 
-resource "aws_instance" "web-server" {
-  ami             = "ami-052efd3df9dad4825"
-  instance_type   = "t2.micro"
-  count           = 2
-  availability_zone = us-east-1a
-  key_name        = "jarvis_privatekey"
-  security_groups = ["${aws_security_group.my_sg.name}"]
-  user_data       = <<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install apache2 -y
-sudo systemctl reload apache2
-sudo mkdir -p /var/www/html/tab/
-sudo echo "hello world, welcome to my tab page $HOSTNAME" >> /var/www/html/tab/index.html 
-EOF
-}
+# resource "aws_instance" "web-server" {
+#   ami             = "ami-052efd3df9dad4825"
+#   instance_type   = "t2.micro"
+#   count           = 2
+#   availability_zone = us-east-1a
+#   key_name        = "jarvis_privatekey"
+#   security_groups = ["${aws_security_group.my_sg.name}"]
+#   user_data       = <<EOF
+# #!/bin/bash
+# sudo apt-get update
+# sudo apt-get install apache2 -y
+# sudo systemctl reload apache2
+# sudo mkdir -p /var/www/html/tab/
+# sudo echo "hello world, welcome to my tab page $HOSTNAME" >> /var/www/html/tab/index.html 
+# EOF
+# }
 
 
 
