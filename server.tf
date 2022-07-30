3d9584753fd87476cce7fbf910b1f12b8fc6b36a
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:40:14 +0530

    --- Auto Git Commit ---

---
+++ server.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  # Configuration options
+  region  = "us-east-1"
+  profile = "jarvis"
+}
+
+resource "aws_instance" "web-server" {
+  ami             = "ami-052efd3df9dad4825"
+  instance_type   = "t2.micro"
+  count           = 2
+  key_name        = "jarvis_privatekey"
+  security_groups = ["${aws_security_group.my_sg.name}"]
+  user_data       = <<EOF
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo systemctl reload apache2
+sudo rm -rf /var/www/html/index.html
+sudo echo "hello everyone $HOSTNAME" >> /var/www/html/index.html
+EOF
+}
+
+
+
+
+
+
+
+
