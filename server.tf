7ef7d33a8231570fb6b8868a106417f24b586dd3
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:41:04 +0530

    --- Auto Git Commit ---

--- server.tf
+++ server.tf
@@ -32,7 +32,7 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl reload apache2
 sudo rm -rf /var/www/html/index.html
-sudo echo "hello everyone $HOSTNAME" >> /var/www/html/index.html
+sudo echo "hello everyone, this is laptop page $HOSTNAME" >> /var/www/html/index.html
 EOF
 }
 
