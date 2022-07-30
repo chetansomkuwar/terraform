1b4d8cc3ffe20b39e5840502d2a377f31640b9fd
chetan <chetansomkuwar45@gmail.com>
2022-07-30 11:41:45 +0530

    --- Auto Git Commit ---

--- server.tf
+++ server.tf
@@ -48,7 +48,7 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo systemctl reload apache2
 sudo rm -rf /var/www/html/index.html
-sudo echo "hello everyone, this is mobile page $HOSTNAME" >> /var/www/html/index.html
+sudo echo "hello everyone, this is tab page $HOSTNAME" >> /var/www/html/index.html
 EOF
 }
 
