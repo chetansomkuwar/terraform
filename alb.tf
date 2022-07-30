53be87b091e6b925e25c66fd242349efc71a7033
chetan <chetansomkuwar45@gmail.com>
2022-07-30 14:39:42 +0530

    --- Auto Git Commit ---

--- alb.tf
+++ alb.tf
@@ -1,9 +1,9 @@
-resource "aws_lb_target_group" "test" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-0d38f6e75bce4c3e6"
-}
+# resource "aws_lb_target_group" "test" {
+#   name     = "tf-example-lb-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = "vpc-0d38f6e75bce4c3e6"
+# }
 
 
 resource "aws_lb" "instance_lb" {
